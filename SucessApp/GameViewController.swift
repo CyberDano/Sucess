03a7af58794051b15f3460962eded0e8b1491a36
DanoMAC <dano@cev.com>
2024-11-07 14:28:51 +0100

    Gameplay

--- SucessApp/GameViewController.swift
+++ SucessApp/GameViewController.swift
@@ -17,7 +17,7 @@ class GameViewController: UIViewController {
     // Componentes del juego
     @IBOutlet weak var ImageSwitch: UIImageView!
     @IBOutlet weak var TextInfo: UITextView!
-    @IBOutlet weak var PlayButton: UIButton!
+    @IBOutlet weak var ChooseButton: UIButton!
     var points: Int = 0 // Contador de puntos
     var Index: Int = -1 // Int para seleccionar un UIImage
     var timer: Timer? // Timer
@@ -32,7 +32,7 @@ class GameViewController: UIViewController {
     }
     /// Muestra información en pantalla
     private func ShowHUD(state: Bool) {
-        PlayButton.isHidden = !state
+        ChooseButton.isHidden = !state
         for n in Options! {
             n.isHidden = !state
         }
@@ -46,20 +46,25 @@ class GameViewController: UIViewController {
             print("valor random: \(tempInt)")
             has = recient.contains(tempInt)
         }
-        recient.append(tempInt)
         if recient.count > 2 {
             timer?.invalidate()
-            print(recient)
-            TextInfo.text = "Choose the sequence:"
-            ShowHUD(state: true)
-            //ImageSwitch.isHidden = true
+            choosing()
+        } else {
+            recient.append(tempInt)
         }
         return tempInt
     }
+    /// Elección de la secuencia del jugador
+    private func choosing() {
+        print(recient)
+        TextInfo.text = "Choose the sequence:"
+        ShowHUD(state: true)
+        ImageSwitch.isHidden = true
+    }
     /// Secuencia de imagenes
-    private func GamePlay(active: Bool) {
+    private func GamePlay() {
         // Iniciar el timer
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: active) { [weak self] _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [weak self] _ in
             guard let self = self else { return }
             TextInfo.text = "Remember the sequence!"
             self.Index = ChangeImage()
@@ -67,9 +72,33 @@ class GameViewController: UIViewController {
             self.ImageSwitch.image = self.images[self.Index]
         }
     }
+    /// Seleccionar el orden de la sucesión (jugador)
+    @IBAction func ChoosedOption(_ sender: UIButton) {
+        print(options)
+        print("Entras en la selección")
+        sender.isSelected = !sender.isSelected
+        if !sender.isSelected {
+            print("Botón pulsado: \(sender.tag)")
+            options.append(sender.tag)
+            sender.backgroundColor = .green
+        }
+        else {
+            sender.backgroundColor = .gray
+        }
+    }
+    
+    @IBAction func CheckResult(_ sender: Any) {
+        if recient == options {
+            points += 1
+            var st: Bool = startedGame()
+        } else {
+            TextInfo.text = "Oh no. You lose :(\nScore: \(points)"
+        }
+    }
     private func startedGame() -> Bool {
-        GamePlay(active: true)
+        GamePlay()
         recient = []
+        options = []
         return true
     }
 }
