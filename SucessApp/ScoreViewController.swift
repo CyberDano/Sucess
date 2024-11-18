55fbf6231f45fa5fee706a226ee882a40c04fdf8
Cristian_Mac <Cristian_git@mac.com>
2024-11-18 12:06:26 +0000

    error fix

--- SucessApp/ScoreViewController.swift
+++ SucessApp/ScoreViewController.swift
@@ -37,6 +37,8 @@ class ScoreViewController: UIViewController, UITableViewDataSource, UITableViewD
             }
             return
         }
+        let score = UserDefaults.standard
+        points = score.integer(forKey: "score")
 
         let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYXZydmtobGJtc2xqZ21ia25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3MjY5MTgsImV4cCI6MjAxNjMwMjkxOH0.Ta-_lXGGwSiUGh0VC8tAFcFQqsqAvB8vvXJjubeQkx8"
         // Datos a enviar en el cuerpo de la solicitud
